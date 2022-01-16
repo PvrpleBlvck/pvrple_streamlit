62ee5657a2e4939ef2e40132cfa41942b60bcf4d
Norest Nyamhunga <41530377+PvrpleBlvck@users.noreply.github.com>
2022-01-16 04:51:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Imports
 # -----------------------------------------------------------
-from scipy.sparse import data
+from scipy import data
 import streamlit as st
 import pandas as pd
 from sklearn.cluster import KMeans
